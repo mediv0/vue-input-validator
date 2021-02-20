492938dd855f7007bc518168f2c96b8f3a1543e3
mediv0 <anonymousaxis@gmail.com>
2021-02-20 20:01:44 +0330

    Test new file definition for prototype methods

--- dev/App.vue
+++ dev/App.vue
@@ -1,14 +1,36 @@
 <template>
     <div id="App">
-        <h1 v-validator>dev component</h1>
+        <input v-validator="props" />
     </div>
 </template>
 
 <script lang="ts">
+import { IchecksProp } from "@/types";
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class App extends Vue {}
+export default class App extends Vue {
+    public props: IchecksProp = {
+        items: [
+            {
+                label: "test1",
+                test: /1/
+            },
+            {
+                label: "test1",
+                test: /1/
+            },
+            {
+                label: "test1",
+                test: /1/
+            }
+        ]
+    };
+
+    public test(): void {
+        this.$validator.showError();
+    }
+}
 </script>
 
 <style lang="scss" scoped></style>
