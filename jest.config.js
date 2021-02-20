f9f66441b40fb0cf78ce9b9d1a3bb3bba47e1f67
mediv0 <anonymousaxis@gmail.com>
2021-02-20 19:41:25 +0330

    Ignore index.ts in component directory

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel"
+    preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+    coverageDirectory: "coverage",
+    collectCoverageFrom: ["./src/**/*.ts", "./src/**/*.tsx", "!./src/index.js", "!./src/index.ts", "!./src/component/index.ts"],
+    collectCoverage: true
 };
