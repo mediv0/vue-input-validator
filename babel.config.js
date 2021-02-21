88891241fbe989a7a6e5d5b92069c7fc4604c23f
mediv0 <anonymousaxis@gmail.com>
2021-02-21 17:21:01 +0330

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,7 @@
 module.exports = {
-    presets: [
-        "@vue/cli-plugin-babel/preset",
-        [
-            "@vue/babel-preset-jsx",
-            {
-                injectH: false
-            }
-        ]
+    presets: ["@vue/cli-plugin-babel/preset", "@babel/preset-typescript", "@vue/babel-preset-jsx"],
+    plugins: [
+        ["@babel/plugin-proposal-decorators", { legacy: true }],
+        ["@babel/plugin-proposal-class-properties", { loose: true }]
     ]
 };
