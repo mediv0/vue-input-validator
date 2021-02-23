a010c1d00280dd0f5885a1313d1eedcb7c029be4
mediv0 <anonymousaxis@gmail.com>
2021-02-23 23:25:09 +0330

    Add validator options example

---
+++ public/validatorOptions.ts
@@ -0,0 +1,43 @@
+import { IchecksProp } from "../src/types/index";
+
+const options: IchecksProp = {
+    
+    key: "key for this object",
+
+
+    items: [
+        {
+            label: "your first test with regex",
+            test: /[0-9]/
+        },
+        {
+            label: "second test  sync function",
+            test: inputValue => {}
+        },
+        {
+            label: "third test with async function",
+            test: async () => {}
+        }
+    ],
+
+    
+    debounce: 200,               // postponed your validation
+    
+    circleSize: 10,              // size of circles in px
+    
+    hideLabels: true,            // hide labels under your input
+    
+    hideLines: true,             // hide lines under your input
+    
+    disable: true,               // disable your input
+    
+    onSuccess: () => {},          // callback when validation passes
+
+
+    onError: {
+        msg: "your error msg",
+        color: "error color if you want it",
+        direction: "ltr", // direction of error,
+        highlight: true,  // highlight error border if set to true
+    }
+};
